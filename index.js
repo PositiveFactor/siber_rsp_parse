0ef6be3c2b7da8c7bfa1b7563c4bf8b0c1396ccd
PositiveFactor <starzed@yandex.ru>
2018-11-13 12:02:27 +0300

    reformat

--- index.js
+++ index.js
@@ -12,58 +12,58 @@ function getFilesList(destPath, extensions){
 }
 
 function saveRspFiles(filename, dir){
-	var rsps = [];
-	fs.readFile(filename, (err, data)=>{
-		if(err){
-			console.log('err', err);
-			return;
-		}
-		
-		var sp = data.toString().split('\n')
-		var startStr = '';
-		sp.forEach(function(item){
-			if(	item.startsWith('RESULT') || 
-				item.startsWith('REALITY_CHECK_AFTER') || 
-				item.startsWith('MESSAGE_TYPE')
-			){
-				startStr = item;
-			}
-			else if(item.startsWith('END')){
-				rsps.push([startStr, item].join('\n'));
-				startStr = '';
-			}
-		})
-		
-		// console.log(rsps);
-		
-		var index = 0;
-		rsps.forEach(function(item){
-			fs.writeFile(path.join(dir, 'rsp_' + index + '.txt'), item, function(err){
-				if(err){
-					console.log('err', err);
-					return;
-				}
-			});
-			index++;
-		})
-	})
+  var rsps = [];
+  fs.readFile(filename, (err, data)=>{
+    if(err){
+      console.log('err', err);
+      return;
+    }
+    
+    var sp = data.toString().split('\n')
+    var startStr = '';
+    sp.forEach(function(item){
+      if(	item.startsWith('RESULT') ||
+      item.startsWith('REALITY_CHECK_AFTER') ||
+      item.startsWith('MESSAGE_TYPE')
+    ){
+      startStr = item;
+    }
+    else if(item.startsWith('END')){
+      rsps.push([startStr, item].join('\n'));
+      startStr = '';
+    }
+  })
+
+  // console.log(rsps);
+
+  var index = 0;
+  rsps.forEach(function(item){
+    fs.writeFile(path.join(dir, 'rsp_' + index + '.txt'), item, function(err){
+      if(err){
+        console.log('err', err);
+        return;
+      }
+    });
+    index++;
+  })
+})
 }
 
 function start(){
-	var list = getFilesList('.', ['txt'])
-	console.log(list);
-	list.forEach( (f) => {
-		let base = f.split('.')[0]
-		let ext = f.split('.')[1]
-		
-		if (fs.existsSync(base)) {
-			console.log(base, 'exist')
-		}
-		else {
-			fs.mkdir(base);
-			saveRspFiles(f, base);
-		}
-	})
+  var list = getFilesList('.', ['txt'])
+  console.log(list);
+  list.forEach( (f) => {
+    let base = f.split('.')[0]
+    let ext = f.split('.')[1]
+
+    if (fs.existsSync(base)) {
+      console.log(base, 'exist')
+    }
+    else {
+      fs.mkdir(base);
+      saveRspFiles(f, base);
+    }
+  })
 }
 
 start();
